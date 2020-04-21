2321cfb34673986f1e5144b7b2c3735102edf0c6
Steve Bunel <therealsteveb@protonmail.com>
2020-04-21 12:51:39 -0500

    Did some more math

--- math.py
+++ math.py
@@ -7,9 +7,8 @@
 # print(list(range(2 ,n+1)))
 
 # math.py
-#   Program to compute the factorial of a number
+#   Program to compute the factorial of a number !!!
 #   Illustrates for loop with an accumulator
-
 # def main():
 #     print("This Program computes the factorial of a number")
 #     n = eval(input("Please enter a whole number: "))
@@ -20,10 +19,20 @@
 
 # main()
 
+# Inport Math !!!
+import math
 
 # Pi
-
-import math
 print(math.pi)
 
+# 4.0 / 10.0 + 3.5 * 2
+print((int(4.0)) / (int(10.0)) + 3.5 * 2)
+
+# 10%4 + 6/2
+print(10%4 + int(6/2))
+
+# abs(4 - 20 // 3) ** 3
+print(abs(4 - 20 // 3) ** 3)
+
+# sqrt(4.5 - 5.0) + 7 * 3
 
